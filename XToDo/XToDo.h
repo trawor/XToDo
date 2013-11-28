f76ecf431ea7783cb5402edd589dce121d7b283c
Travis <travis@avos.com>
2013-11-28 10:43:29 +0800

    Initial Commit

---
+++ XToDo/XToDo.h
@@ -0,0 +1,13 @@
+//
+//  XToDo.h
+//  XToDo
+//
+//  Created by Travis on 13-11-28.
+//  Copyright (c) 2013年 Plumn LLC. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface XToDo : NSObject
+
+@end
---
+++ XToDo/XToDo.m
@@ -0,0 +1,64 @@
+//
+//  XToDo.m
+//  XToDo
+//
+//  Created by Travis on 13-11-28.
+//    Copyright (c) 2013年 Plumn LLC. All rights reserved.
+//
+
+#import "XToDo.h"
+
+static XToDo *sharedPlugin;
+
+@interface XToDo()
+
+@property (nonatomic, strong) NSBundle *bundle;
+@end
+
+@implementation XToDo
+
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
+    static id sharedPlugin = nil;
+    static dispatch_once_t onceToken;
+    NSString *currentApplicationName = [[NSBundle mainBundle] infoDictionary][@"CFBundleName"];
+    if ([currentApplicationName isEqual:@"Xcode"]) {
+        dispatch_once(&onceToken, ^{
+            sharedPlugin = [[self alloc] initWithBundle:plugin];
+        });
+    }
+}
+
+- (id)initWithBundle:(NSBundle *)plugin {
+{
+    if (self = [super init]) {
+        // reference to plugin's bundle, for resource acccess
+        self.bundle = plugin;
+        
+        // Create menu items, initialize UI, etc.
+
+        // Sample Menu Item:
+        NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"File"];
+        if (menuItem) {
+            [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
+            NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
+            [actionMenuItem setTarget:self];
+            [[menuItem submenu] addItem:actionMenuItem];
+        }
+    }
+    return self;
+}
+
+// Sample Action, for menu item:
+- (void)doMenuAction
+{
+    NSAlert *alert = [NSAlert alertWithMessageText:@"Hello, World" defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
+    [alert runModal];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+@end
