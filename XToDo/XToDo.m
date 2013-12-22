80b1e039a3de2eb68d6203bf286804e9beea5111
Travis <i@imi.im>
2013-12-22 16:26:24 +0800

    add the Snippets support
    close #5

--- XToDo/XToDo.m
+++ XToDo/XToDo.m
@@ -52,8 +52,19 @@ static XToDo* sharedPlugin=nil;
             [actionMenuItem setTarget:self];
             [[menuItem submenu] addItem:actionMenuItem];
             
+            
+            //add Snippet Group
+            NSMenu *submenu = [[NSMenu alloc] init];
+            
+            NSMenuItem *mainItem = [[NSMenuItem alloc] init];
+            [mainItem setTitle:@"Snippets"];
+            
+            [mainItem setSubmenu:submenu];
+            [[menuItem submenu] addItem:mainItem];
+            
+            
             //support snippets to add TODO
-            actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"ToDo Snippet"
+            actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"TODO"
                                                          action:@selector(insertToDo)
                                                   keyEquivalent:@"t"];
             
@@ -61,7 +72,42 @@ static XToDo* sharedPlugin=nil;
             
             
             [actionMenuItem setTarget:self];
-            [[menuItem submenu] addItem:actionMenuItem];
+            [submenu addItem:actionMenuItem];
+            
+            
+            //support snippets to add FIXME
+            actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"FIXME"
+                                                        action:@selector(insertFixMe)
+                                                 keyEquivalent:@"x"];
+            
+            [actionMenuItem setKeyEquivalentModifierMask:NSControlKeyMask|NSShiftKeyMask];
+            
+            
+            [actionMenuItem setTarget:self];
+            [submenu addItem:actionMenuItem];
+            
+            
+            //support snippets to add !!!
+            actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"!!!"
+                                                        action:@selector(insertWarn)
+                                                 keyEquivalent:@"1"];
+            
+            [actionMenuItem setKeyEquivalentModifierMask:NSControlKeyMask|NSShiftKeyMask];
+            
+            
+            [actionMenuItem setTarget:self];
+            [submenu addItem:actionMenuItem];
+            
+            
+            //support snippets to add ???
+            actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"???"
+                                                        action:@selector(insertAsk)
+                                                 keyEquivalent:@"q"];
+            
+            [actionMenuItem setKeyEquivalentModifierMask:NSControlKeyMask|NSShiftKeyMask];
+
+            [actionMenuItem setTarget:self];
+            [submenu addItem:actionMenuItem];
         }
     }
     return self;
@@ -99,16 +145,32 @@ static XToDo* sharedPlugin=nil;
 }
 
 -(void)insertToDo{
-    NSString *cmt=[NSString stringWithFormat:@"//%@: ",@"TODO"];
+    NSString *cmt=[NSString stringWithFormat:@"//%@:",@"TODO"];
     [self insertComment:cmt];
+    
+}
+-(void)insertFixMe{
+    NSString *cmt=[NSString stringWithFormat:@"//%@:",@"FIXME"];
+    [self insertComment:cmt];
+    
 }
 
+-(void)insertWarn{
+    NSString *cmt=[NSString stringWithFormat:@"//%@:",@"!!!"];
+    [self insertComment:cmt];
+    
+}
+-(void)insertAsk{
+    NSString *cmt=[NSString stringWithFormat:@"//%@:",@"???"];
+    [self insertComment:cmt];
+    
+}
 -(void)insertComment:(NSString*)cmt{
     IDESourceCodeEditor *editor=[XToDoModel currentEditor];
     NSTextView *textView=editor.textView;
     if (textView) {
-        //FIXME: stop the input course change line
         [textView insertText:cmt];
+        [textView insertText:@" "];
     }
 }
 
