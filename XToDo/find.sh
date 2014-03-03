591eeb4d2586990c9c6ef678ef1675fb38f1a714
Till Toenshoff <toenshoff@me.com>
2014-03-03 18:51:26 +0100

    Now supports ObjectiveC, ObjectiveC++, C and C++

--- XToDo/find.sh
+++ XToDo/find.sh
@@ -8,4 +8,4 @@
 
 KEYWORDS="$2"
 
-find "$1" \( -name "*.h" -or -name "*.m" \) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "//\ {0,2}($KEYWORDS).*\$" | perl -p -e "s/($KEYWORDS)/:\$1/"
+find "$1" \( -name "*.h" -or -name "*.hpp" -or -name "*.m" -or -name "*.mm" -or -name "*.c" -or -name "*.cpp" -or -name "*.cc"    \) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "//\ {0,2}($KEYWORDS).*\$" | perl -p -e "s/($KEYWORDS)/:\$1/"
