21d0bdf033427534eb76a85bffa290215a6cb72a
Travis <i@imi.im>
2014-03-07 12:12:08 +0800

    support .cpp .mm files , and ignore Pods folder
    close #18

--- XToDo/find.sh
+++ XToDo/find.sh
@@ -8,4 +8,4 @@
 
 KEYWORDS="$2"
 
-find "$1" \( -name "*.h" -or -name "*.m" \) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "//\ {0,2}($KEYWORDS).*\$" | perl -p -e "s/($KEYWORDS)/:\$1/"
+find "$1" -not -path "*/Pods/*" \( -name "*.h" -or -name "*.m" -or -name "*.cpp" -or -name "*.mm" \) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "//\ {0,2}($KEYWORDS).*\$" | perl -p -e "s/($KEYWORDS)/:\$1/"
